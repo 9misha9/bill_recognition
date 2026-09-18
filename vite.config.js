05c01108f54a672bc0df8868a86c683ef17bdd49
Mykhailo Vidlyvanyi <134156241+9misha9@users.noreply.github.com>
2026-09-18 15:57:23 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    host: '127.0.0.1',
-    port: 5173,
-    open: false
-  },
-  build: {
-    outDir: 'dist',
-    minify: 'esbuild'
-  }
-});
-
-export default defineConfig({
   base: '/bill_recognition/',
 
   server: {
