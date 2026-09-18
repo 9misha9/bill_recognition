96bb6e3618c7356f9fe839bbba80a299aefa1ebe
Mykhailo Vidlyvanyi <134156241+9misha9@users.noreply.github.com>
2026-09-18 15:54:00 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,3 +11,18 @@ export default defineConfig({
     minify: 'esbuild'
   }
 });
+
+export default defineConfig({
+  base: '/bill_recognition/',
+
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    open: false
+  },
+
+  build: {
+    outDir: 'dist',
+    minify: 'esbuild'
+  }
+});
